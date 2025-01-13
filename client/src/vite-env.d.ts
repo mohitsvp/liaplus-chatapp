97cb1250a941cc74e96703ba0fec0fc9c9c89a4f
Mohit Singhal <singhalmohit011@gmail.com>
2025-01-13 09:32:49 +0530

    Added env vite

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_API_URL: string; // Add your environment variables here
+    readonly NODE_ENV: string; // Example
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
+  
